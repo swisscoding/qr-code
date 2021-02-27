d3af62aea0e4a165b9ab47cc1db252c408d71814
SwissCoding | Python <74186883+swisscoding@users.noreply.github.com>
2021-02-27 21:34:59 +0100

    Update qr-code.py

--- qr-code.py
+++ qr-code.py
@@ -1,13 +1,15 @@
 #!/usr/local/bin/python3
+# Made by @swisscoding on Instagram
 
-import pyqrcode
-import colored
+import pyqrcode, colored
 
 print(colored.stylize("\n---- | Create QR-Code | ----\n", colored.fg("red")))
 
+# url for the qr code
 url = input("Type in the URL: ")
 print()
 
+# creation
 qrCode = pyqrcode.create(url, error="L", version=3)
-qrCode.png('QR-Code.png', scale=20)
+qrCode.png("QR-Code.png", scale=20)
 qrCode.show()
